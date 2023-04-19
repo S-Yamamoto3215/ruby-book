c788ff2b270040bc05b2fb9da8289d51b7b38d40
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-04-19 17:51:19 +0900

    wait settings

--- web_driver.rb
+++ web_driver.rb
@@ -8,15 +8,20 @@ account = read_account(ARGV[0])
 
 
 driver = Selenium::WebDriver.for :chrome
+wait = Selenium::WebDriver::Wait.new(timeout: 20)
+
 driver.get 'https://www.amazon.co.jp/'
 element = driver.find_element(:id, 'nav-link-accountList')
 element.click
 
+wait.until { driver.find_element(:id, 'ap_email').displayed? }
 element = driver.find_element(:id, 'ap_email')
 element.send_keys(account[:email])
 
 element = driver.find_element(:id, 'continue')
 element.click
+
+wait.until { driver.find_element(:id, 'ap_password').displayed? }
 
 sleep 3
 driver.quit
