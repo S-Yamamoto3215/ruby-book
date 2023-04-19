2e078c9e4bdc089beaf3d9b1b8766b8c04fd612e
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-04-19 17:44:56 +0900

    E-mail input by WebDriver.

--- web_driver.rb
+++ web_driver.rb
@@ -1,11 +1,22 @@
 # fronzen_string_literal: true
 
 require 'selenium-webdriver'
+require_relative './account_reader'
+
+abort 'Usage: ruby web_driver.rb <account.json>' if ARGV.empty?
+account = read_account(ARGV[0])
+
 
 driver = Selenium::WebDriver.for :chrome
 driver.get 'https://www.amazon.co.jp/'
 element = driver.find_element(:id, 'nav-link-accountList')
-puts element.text
 element.click
+
+element = driver.find_element(:id, 'ap_email')
+element.send_keys(account[:email])
+
+element = driver.find_element(:id, 'continue')
+element.click
+
 sleep 3
 driver.quit
