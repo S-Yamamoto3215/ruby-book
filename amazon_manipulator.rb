49fd346eff70ed3f861e0bd376348e217f734b45
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-04-25 16:05:55 +0900

    login method

--- amazon_manipulator.rb
+++ amazon_manipulator.rb
@@ -15,36 +15,35 @@ class AmazonManipulator
     @account = read(account_file)
   end
 
-  def run
+  def login
     @driver.get BASE_URL
     element = @driver.find_element(:id, 'nav-link-accountList')
     element.click
-
     @wait.until { @driver.find_element(:id, 'ap_email').displayed? }
     element = @driver.find_element(:id, 'ap_email')
     element.send_keys(@account[:email])
-
     element = @driver.find_element(:id, 'continue')
     element.click
-
     @wait.until { @driver.find_element(:id, 'ap_password').displayed? }
     element = @driver.find_element(:id, 'ap_password')
     element.send_keys(@account[:password])
-
     element = @driver.find_element(:id, 'signInSubmit')
     element.click
+    @wait.until { @driver.find_element(:id, 'nav-link-accountList').displayed? }
+  end
+
+  def run
+    login
 
     @wait.until { @driver.find_element(:id, 'nav-orders').displayed? }
     element = @driver.find_element(:id, 'nav-orders')
     element.click
     @wait.until { @driver.find_element(:id, 'navFooter').displayed? }
     puts @driver.title
-
     years = @driver.find_element(:id, 'time-filter')
     select = Selenium::WebDriver::Support::Select.new(years)
     select.select_by(:value, 'year-2022')
     @wait.until { @driver.find_element(:id, 'navFooter').displayed? }
-
     selector = '#ordersContainer .order > div:nth-child(2) .a-fixed-left-grid-col.a-col-right > div:nth-child(1)'
     titles = @driver.find_elements(:css, selector)
     puts "アイテム数: #{titles.size}"
