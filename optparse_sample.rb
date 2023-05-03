f12724d6b6d75f05fc36be424bb449436058bc2b
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-05-04 00:49:44 +0900

    optparse Additional YEAR options.

--- optparse_sample.rb
+++ optparse_sample.rb
@@ -8,9 +8,15 @@ opts.program_name = 'optparse_sample'
 opts.version = [0, 2]
 opts.release = '2023-05-04'
 
+opts.on('-y YEAR', '--year YEAR', '注文履歴を取得する年を指定する') do |y|
+  @order_year = y
+end
+
 opts.on_tail('-v', '--version', 'バージョンを表示する') do
   puts opts.ver
   exit
 end
 
 opts.parse!(ARGV)
+
+puts "注文履歴を取得する年: #{@order_year}"
